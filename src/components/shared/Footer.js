775cb8de17c875834353711a2bdf3663ac23b0ee
david prorok <davidjamesprorok@gmail.com>
2021-04-15 14:40:36 -0700

    Added footer with buttons using default links

---
+++ src/components/shared/Footer.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { SocialIcon } from 'react-social-icons';
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: center;
+  padding: 20px 0px;
+  bottom: 0;
+
+  .social-icon {
+    margin: 0 8px;
+  }
+`;
+
+function SocialIconWrapper (props) {
+  return <SocialIcon className="social-icon" fgColor="white" {...props} />;
+}
+
+// TODO: Add in correct links
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <SocialIconWrapper url="https://twitter.com/" className="social-icon" />
+      <SocialIconWrapper url="https://instagram.com" className="social-icon" />
+      <SocialIconWrapper url="https://discord.com" className="social-icon" />
+      <SocialIconWrapper url="https://reddit.com" className="social-icon" />
+      <SocialIconWrapper url="https://rarible.com" className="social-icon" />
+    </FooterContainer>
+  );
+}
+
+export default Footer;
