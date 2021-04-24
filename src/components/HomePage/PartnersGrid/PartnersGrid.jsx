149f33cfaa09657a4fbd01ece3e0fc0c809c9ccb
david prorok <davidjamesprorok@gmail.com>
2021-04-23 21:50:53 -0700

    make partners grid a little better

--- src/components/HomePage/PartnersGrid/PartnersGrid.jsx
+++ src/components/HomePage/PartnersGrid/PartnersGrid.jsx
@@ -18,59 +18,65 @@ import vocadoventures from '../../../images/partnersGrid/vocadoventures.png';
 import universe from '../../../images/partnersGrid/universe.png';
 
 const gridImages = [
-    {src: panvala, alt: 'panvala', href:'https://panvala.com/'},
-    {src: resonate, alt: 'resonate', href:'https://resonate.is/'},
-    {src: audius, alt: 'audius' , href:'https://audius.co/afuturemodern'},
-    {src: bridgit, alt: 'bridgit', href: "#"},
-    {src: abridged, alt: 'abridged', href: 'https://abridged.io/'},
-    {src: roll, alt: 'roll', href: 'https://app.tryroll.com/token/BUILD'},
-    {src: api3, alt: 'api3', href:'https://www.api3.org/'},
-    {src: handshake, alt: 'handshake', href: 'https://handshake.org'},
-    {src: brightid, alt: 'brightid', href: 'https://www.brightid.org'},
-    {src: fantastic, alt: 'fantastic', href: '#'},
-    {src: pictureplane, alt: 'pictureplane', href: '#'},
-    {src: phlote, alt: 'phlote', href: 'https://www.phlote.co/'},
-    {src: codame, alt: 'codame', href: 'codame.com'},
-    {src: vocadoventures, alt: 'vocadoventures', href: 'https://codame.com'},
-    {src: universe, alt: 'universe', href: 'https://universecontemporary.art/'},
- ]
+    { src: panvala, alt: 'panvala', href: 'https://panvala.com/' },
+    { src: resonate, alt: 'resonate', href: 'https://resonate.is/' },
+    { src: audius, alt: 'audius', href: 'https://audius.co/afuturemodern' },
+    { src: bridgit, alt: 'bridgit', href: "#" },
+    { src: abridged, alt: 'abridged', href: 'https://abridged.io/' },
+    { src: roll, alt: 'roll', href: 'https://app.tryroll.com/token/BUILD' },
+    { src: api3, alt: 'api3', href: 'https://www.api3.org/' },
+    { src: handshake, alt: 'handshake', href: 'https://handshake.org' },
+    { src: brightid, alt: 'brightid', href: 'https://www.brightid.org' },
+    { src: fantastic, alt: 'fantastic', href: '#' },
+    { src: pictureplane, alt: 'pictureplane', href: '#' },
+    { src: phlote, alt: 'phlote', href: 'https://www.phlote.co/' },
+    { src: codame, alt: 'codame', href: 'codame.com' },
+    { src: vocadoventures, alt: 'vocadoventures', href: 'https://codame.com' },
+    { src: universe, alt: 'universe', href: 'https://universecontemporary.art/' },
+]
 
 
 export const PartnersGrid = () => {
-    
-    const PartnersGridContainer = styled.div`
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
-        gap: 15px 25px;
-        margin: 10px auto;
-        width: 30vw;
-        justify-items: center;
-        justify-content: center;
-        background: #ffffffaa;
-        align-items: center;
-        padding-bottom: 6vh;
 
+    const PartnersGridContainer = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    display: grid;
+    grid-gap: 1rem;
+
+    @media (min-width: 600px) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      @media (min-width: 900px) {
+        grid-template-columns: repeat(3, 1fr);
+      }
     `
 
-    const images = gridImages.map(({alt, src, href}, i) => 
-    <a href={href}  >
-        <img 
-                        src={src}
-                        alt={alt}
-                        key={'partnersCell-' + i} 
-                        className="partnersCell" 
-                        style={{
-                            background: '#D4DCFB',
-                            borderRadius: '36px',
-                            width: '9rem',
-                            height: '5vh',
-                            padding: '.5rem 2rem',
-                            justifySelf: 'stretch',
-                            objectFit: 'contain'
-                    }}/>
-    </a>
-                )
+    const images = gridImages.map(({ alt, src, href }, i) =>
+        <div
+            key={'partnersCell-' + i}
+            style={{
+                padding: '1rem',
+            }}
+        >
+            <a href={href}  >
+                <img
+                    src={src}
+                    alt={alt}
+                    className="partnersCell"
+                    style={{
+                        background: '#D4DCFB',
+                        borderRadius: '36px',
+                        width: '100%',
+                        height: '3rem',
+                        padding: '.5rem 2rem',
+                        justifySelf: 'stretch',
+                        objectFit: 'contain'
+                    }} />
+            </a>
+        </div>
+    )
     return (
         <PartnersGridContainer>
             {images}
