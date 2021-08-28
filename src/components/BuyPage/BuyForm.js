c1e1cfa8cf1bc60f61b981656bdef317d2e2be7e
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2021-08-28 12:38:08 -0600

    added button

--- src/components/BuyPage/BuyForm.js
+++ src/components/BuyPage/BuyForm.js
@@ -1,33 +1,63 @@
-import { PayPalScriptProvider, PayPalButtons, FUNDING } from "@paypal/react-paypal-js";
+import styled from 'styled-components';
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  FUNDING,
+} from '@paypal/react-paypal-js';
 
+const PAYPAL_CLIENT_ID =
+  'AWdHpI51fVvKquVN3KDB6wi-S_ad7Hm-eElY6tAJbCwlSDKoW7tOtZI1q8z8wW7isA_AuRimSoimd-12';
 
-
-const PAYPAL_CLIENT_ID = "AWdHpI51fVvKquVN3KDB6wi-S_ad7Hm-eElY6tAJbCwlSDKoW7tOtZI1q8z8wW7isA_AuRimSoimd-12";
-
-const paypay_button_style = {
-  color: "blue",
-  shape: "pill",
- }
+const paypal_button_style = {
+  color: 'blue',
+  shape: 'pill',
+};
+const card_button_style = {
+  color: 'black',
+  shape: 'pill',
+};
 
 const paypal_options = {
-  "client-id": PAYPAL_CLIENT_ID,
-  currency: "USD",
-  intent: "capture",
+  'client-id': PAYPAL_CLIENT_ID,
+  currency: 'USD',
+  intent: 'capture',
 };
 
-const paypal_container_style = {
-  margin: 40,
-};
+const PaypalContainerStyle = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const CardContainerStyle = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem;
+  line-height: 1.5rem;
+  font-size: 1.3rem;
+`;
 
 const BuyForm = () => {
   return (
     <PayPalScriptProvider options={paypal_options}>
-        <div style={paypal_container_style}>
-            <PayPalButtons
-                style={paypay_button_style}
-                fundingSource={FUNDING.PAYPAL}
-            />
-        </div>
+      <Container>
+        <PaypalContainerStyle>
+          <PayPalButtons
+            style={paypal_button_style}
+            fundingSource={FUNDING.PAYPAL}
+          />
+        </PaypalContainerStyle>
+
+        <CardContainerStyle>
+          <PayPalButtons
+            style={card_button_style}
+            fundingSource={FUNDING.CARD}
+          />
+        </CardContainerStyle>
+      </Container>
     </PayPalScriptProvider>
   );
 };
